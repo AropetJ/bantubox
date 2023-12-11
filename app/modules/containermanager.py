31542e52fd7219de265fb140afc766da431684ef
AropetJ <aropetjoel32@gmail.com>
2023-12-11 03:27:25 +0300

    Added containermanager.py

--- app/modules/containermanager.py
+++ app/modules/containermanager.py
@@ -7,10 +7,78 @@ Usage:
         sudo ./bb.py run <commad> <options>
 """
 
-import os #file and process management
-import uuid #unique container ids
-import tarfile #extracting the image tarball file
+import os
+import uuid
+import tarfile
 import stat
-import click #command line utility
-import linux #linux sys call wrappers
+import click
+from containerutils import ContainerUtils
+import linux
 
+class ContainerManager:
+    """
+    Base class for creating and managing containers
+    """
+    def __init__(self, image_name, image_dir, container_dir):
+        self.image_name = image_name
+        self.image_dir = image_dir
+        self.container_dir = container_dir
+        self.container_id = str(uuid.uuid4())
+    
+
+    def create_container_root(image_name, image_dir, container_id, container_dir):
+        """
+        This function is used to set up the root filesystem for a container. It
+        takes the name of the image to use, the directory containing the image, the
+        ID of the container, and the directory to store the container data.
+        
+        If the image rootfs does not already exist, this function extracts the image
+        tarball to create it. It then creates the necessary directories for the 
+        container's overlay mount and performs the overlay mount.
+
+        Args:
+            image_name (str): The name of the image to use.
+            image_dir (str): The directory containing the image.
+            container_id (str): The ID of the container.
+            container_dir (str): The directory to store the container data.
+
+
+        Returns:
+            str: The path to the container's root filesystem.
+        """
+        image_path = ContainerUtils._get_image_path(image_name, image_dir)
+    
+        image_root = os.path.join(image_dir, image_name, 'rootfs')
+
+        assert os.path.exists(image_path), f"Unable to locate image {image_name}"
+
+        if not os.path.exists(image_root):
+            os.makedirs(image_root)
+            with tarfile.open(image_path) as tar_file:
+                all_members = tar_file.getmembers()
+
+                filtereed_members = []
+                for member in all_members:
+                    if member.type not in (tarfile.CHRTYPE, tarfile.BLKTYPE):
+                        filtereed_members.append(member)
+                tar_file.extractall(image_root, members=filtereed_members)
+
+        container_cow_rw = ContainerUtils._get_container_path(
+            container_id, container_dir, 'cow_rw')
+        container_cow_workdir = ContainerUtils._get_container_path(
+            container_id, container_dir, 'cow_workdir')
+        container_rootfs = ContainerUtils._get_container_path(
+            container_id, container_dir, 'rootfs')
+    
+        for d in (container_cow_rw, container_cow_workdir, container_rootfs):
+            if not os.path.exists(d):
+                os.makedirs(d)
+
+        linux.mount(
+            'overlay', container_rootfs, 'overlay', linux.MS_NODEV,
+            "lowerdir={image_root},upperdir={cow_rw},workdir={cow_workdir}".format(
+                image_root=image_root,
+                cow_rw=container_cow_rw,
+                cow_workdir=container_cow_workdir))
+    
+        return container_rootfs
--- app/modules/containerutils.py
+++ app/modules/containerutils.py
@@ -7,14 +7,15 @@ Usage:
         sudo ./bb.py run <commad> <options>
 """
 
-import os #file and process management
-import uuid #unique container ids
-import tarfile #extracting the image tarball file
+import os
+import tarfile
 import stat
-import click #command line utility
-import linux #linux sys call wrappers
+import linux
 
 class ContainerUtils:
+    """
+    Base class for handling container utilities like directories and device files
+    """
     @staticmethod
     def _get_image_path(image_name, image_dir, image_suffix='tar'):
         """Returns a path to a directory containing the base image
@@ -24,7 +25,7 @@ class ContainerUtils:
             image_dir (str): The directory in which the image is located
             image_suffix (str, optional): The extension of the image file. Defaults to 'tar'.
         """
-        return os.path.join(image_dir, image_name + os.extsep + image_suffix) #creates a path image_dir/image_name.tar
+        return os.path.join(image_dir, image_name + os.extsep + image_suffix)
     
 
     @staticmethod
@@ -35,7 +36,7 @@ class ContainerUtils:
             container_id (str): The container ID
             container_dir (str): The directory in which the container will be located
         """
-        return os.path.join(container_dir, container_id, *subdir_names) #Creates a path to container dir container_dir/container_id/subdirs
+        return os.path.join(container_dir, container_id, *subdir_names)
     
 
     @staticmethod
@@ -45,10 +46,9 @@ class ContainerUtils:
         Args:
             dev_path (str): A path to the device nodes
         """
-        for i, dev in enumerate(['stdin', 'stdout', 'stderr']): #iterate through the list, assign each value an index
-            os.symlink(f'/proc/self/fd/{i}', os.path.join(dev_path, dev)) #creates a symbolic link pointing from /proc/self/fd/i (which is typically the stdin, stdout and stderr) to the file inside the dev directory of the container's new root.
-        os.symlink('/proc/self/fd', os.path.join(dev_path, 'fd'))
-        #Adding some extra devices
+        for i, dev in enumerate(['stdin', 'stdout', 'stderr']):
+            os.symlink(f'/proc/self/fd/{i}', os.path.join(dev_path, dev))
+
         DEVICES = {'null': (stat.S_IFCHR, 1, 3), 'zero': (stat.S_IFCHR, 1, 5),
                    'random': (stat.S_IFCHR, 1, 8), 'urandom': (stat.S_IFCHR, 1,9),
                    'console': (stat.S_IFCHR, 136, 1), 'tty': (stat.S_IFCHR, 5, 0),
